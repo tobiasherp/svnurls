f6e6b143a1f10ba942b78558be947c55beef5928
Tobias Herp <tobias.herp@stein.de>
2019-11-12 10:07:51 +0100

    Plain Python classifier added

--- setup.py
+++ setup.py
@@ -94,6 +94,7 @@ setup(name='svnurls'
                             ]
     , include_package_data=True
     , classifiers=[
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
